2550b20513ea7c1b91f904f94f698dff247a3c3d
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2022-06-07 08:22:50 +0300

    yes

--- src/pages/Ras.js
+++ src/pages/Ras.js
@@ -3,6 +3,10 @@ import Axios from "axios";
 import Modal from "react-modal";
 
 function Ras() {
+  const [in1, setin1] = useState();
+  const [in2, setin2] = useState();
+  const [in3, setin3] = useState();
+
   const [modalIsOpen, setIsOpen] = useState(false);
 
   const [items, setitems] = useState([
@@ -56,7 +60,156 @@ function Ras() {
         style={customStyles}
         contentLabel="Example Modal"
       >
-        asdasd
+        <div style={{}}>
+          <h2 style={{ fontSize: "30pt", textAlign: "center" }}>
+            טופס הוספת משתמש חדש:
+          </h2>
+          <br /> <br />
+          <div style={{ display: "flex" }}>
+            <div style={{ width: "50%", textAlign: "center" }}>
+              <h2>מספר אישי:</h2>
+              <br />
+              <h2>כינוי:</h2>
+              <br />
+              <h2>סיסמה ראשונית:</h2>
+              <br />
+            </div>
+            <div style={{ width: "50%", float: "left", textAlign: "center" }}>
+              <br />{" "}
+              <input
+                onChange={(e) => {
+                  setin1(e.target.value);
+                }}
+                value={in1}
+                style={{
+                  textAlign: "center",
+                  fontSize: "18pt",
+                  width: "70%",
+                  height: "23px",
+                }}
+              />
+              <br /> <br />
+              <br /> <br />
+              <input
+                onChange={(e) => {
+                  setin2(e.target.value);
+                }}
+                value={in2}
+                style={{
+                  textAlign: "center",
+                  fontSize: "18pt",
+                  width: "70%",
+                  height: "23px",
+                }}
+              />
+              <br /> <br />
+              <br /> <br />
+              <input
+                onChange={(e) => {
+                  setin3(e.target.value);
+                }}
+                value={in3}
+                style={{
+                  textAlign: "center",
+                  fontSize: "18pt",
+                  width: "30%",
+                  height: "23px",
+                }}
+              />{" "}
+              <button
+                onClick={() => {
+                  var result = "";
+                  var characters =
+                    /* "ABCDEFGHIJKLMNOPQRSTUVWXYZ4567890abcdefghijklmnopqrstuv" +*/ "wxyz123";
+                  var charactersLength = characters.length;
+                  for (var i = 0; i < 6; i++) {
+                    result += characters.charAt(
+                      Math.floor(Math.random() * charactersLength)
+                    );
+                  }
+                  setin3(result);
+                }}
+                style={{ width: "20%", fontSize: "18pt" }}
+              >
+                צור קל
+              </button>
+              <button
+                onClick={() => {
+                  var result = "";
+                  var characters =
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ4567890abcdefghijklmnopqrstuv" +
+                    "wxyz123";
+                  var charactersLength = characters.length;
+                  for (var i = 0; i < 10; i++) {
+                    result += characters.charAt(
+                      Math.floor(Math.random() * charactersLength)
+                    );
+                  }
+                  setin3(result);
+                }}
+                style={{ width: "20%", fontSize: "18pt" }}
+              >
+                צור קשה
+              </button>
+            </div>
+          </div>
+          <br />
+          <br />
+          <br />
+          <button
+            className="footercancelbutton"
+            onClick={() => {
+              closeModal();
+            }}
+            style={{
+              fontSize: "16pt",
+              width: "300px",
+              height: "50px",
+            }}
+          >
+            בטל
+          </button>
+          <button
+            style={{
+              width: "87px",
+              height: "50px",
+              backgroundColor: "unset",
+              cursor: "unset",
+            }}
+          ></button>
+          <button
+            onClick={async () => {
+              if (/* await send() */ true) {
+                setin1("");
+                setin2("");
+                setin3("");
+              } else {
+              }
+            }}
+            style={{ fontSize: "16pt", width: "300px", height: "50px" }}
+          >
+            שמור והוסף משתמש חדש
+          </button>
+          <button
+            style={{
+              width: "87px",
+              height: "50px",
+              backgroundColor: "unset",
+              cursor: "unset",
+            }}
+          ></button>
+          <button
+            onClick={async () => {
+              if (/* await send() */ true) {
+                closeModal();
+              } else {
+              }
+            }}
+            style={{ fontSize: "16pt", width: "300px", height: "50px" }}
+          >
+            שמור וחזור לספר טלפונים
+          </button>
+        </div>
       </Modal>
       <button
         style={{
