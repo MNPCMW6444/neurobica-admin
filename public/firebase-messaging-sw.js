03a2074f14ad0796faafea9da3537702c00142a3
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2022-06-11 00:46:00 +0300

    asd

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js");
+firebase.initializeApp({
+  apiKey: "AIzaSyAsDaTlSLrVbHM8M1MkVgG7Dle_9866CCE",
+  authDomain: "neurobica-admin.firebaseapp.com",
+  projectId: "neurobica-admin",
+  storageBucket: "neurobica-admin.appspot.com",
+  messagingSenderId: "406250116393",
+  appId: "1:406250116393:web:de865ac78ec5d0360f9338",
+  measurementId: "G-T8PLVEN74G",
+});
+const messaging = firebase.messaging();
