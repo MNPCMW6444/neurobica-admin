4de6b3fbfe4a2398adb41d77bff349ca55c79281
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-07-17 19:35:16 +0300

    wed

--- public/sw.js
+++ public/sw.js
@@ -5,11 +5,10 @@ self.addEventListener("install", (e) => {
       .then((cache) => cache.addAll(["/index.html", "/index.js"]))
   );
 });
-/* 
+
 self.addEventListener("fetch", (e) => {
   console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
- */
